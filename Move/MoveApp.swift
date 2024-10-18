46750757b9ce4dd4d819d5ae9e82e47d728d37f2
이혜빈 <bin020129@gmail.com>
2024-10-18 15:45:00 +0900

    Initial Commit

---
+++ Move/MoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveApp.swift
+//  Move
+//
+//  Created by 이혜빈 on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoveTests/MoveTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoveTests.swift
+//  MoveTests
+//
+//  Created by 이혜빈 on 10/18/24.
+//
+
+import Testing
+@testable import Move
+
+struct MoveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
